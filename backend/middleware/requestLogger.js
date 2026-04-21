3a30951c7405e48509c393521685637761cf23e6
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2026-04-21 11:40:17 +0530

    feat: implement request logger middleware to track API requests and responses

---
+++ backend/middleware/requestLogger.js
@@ -0,0 +1,18 @@
+const logger = require('../utils/logger');
+
+const requestLogger = (req, res, next) => {
+    const startTime = Date.now();
+
+    logger.info(`Incoming request: ${req.method} ${req.originalUrl} from ${req.ip}`);
+
+    const originalEnd = res.end;
+    res.end= function (chunk,encoding){
+        const duration = Date.now() - startTime;
+
+        logger.info(`Response completed ${req.method} ${req.originalUrl} - ${res.statusCode} - ${duration}ms`);
+        originalEnd.call(this, chunk, encoding)
+    }
+    next();
+}
+
+module.exports = requestLogger;
