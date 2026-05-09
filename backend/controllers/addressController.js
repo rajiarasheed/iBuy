215b50196df3820a0e022c0d7613052b55b20d35
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2026-05-09 11:10:30 +0530

    created address controller

---
+++ backend/controllers/addressController.js
@@ -0,0 +1,32 @@
+const { AddressService } = require('../services');
+const { addressValidation } = require('../utils/validation')
+const BaseController=require('./BaseController')
+class AddressController extends BaseController{
+    // CREATE ADDRESS
+    static createAddress=BaseController.asyncHandler(async(req,res)=>{
+        const validatedData=BaseController.validateRequest(addressValidation,req.body);
+        const address=await AddressService.createAddress(req.user._id,validatedData);
+        BaseController.logAction('ADDRESS_CREATED', address.user);
+        BaseController.sendSuccess(res,'Address created successfully!',address,201)
+    })
+    // GET ALL ADDRESS
+    static getAddresses=BaseController.asyncHandler(async(req,res)=>{
+        const address=await AddressService.getUserAddresses(req.user._id);
+        BaseController.logAction('GET_ALL_ADDRESSES', address.user);
+        BaseController.sendSuccess(res,'Address fetched successfully!',address,200)
+    })
+    // UPDATE ADDRESS
+    static updateAddress=BaseController.asyncHandler(async(req,res)=>{
+        const address=await AddressService.updateAddress(req.user._id,req.params.id,req.body);
+        BaseController.logAction('UPDATE_ADDRESS', address.user);
+        BaseController.sendSuccess(res,'Address updated successfully!',address,200)
+    })
+    // DELETE ADDRESS
+    static deleteAddress=BaseController.asyncHandler(async(req,res)=>{
+        const address=await AddressService.deleteAddress(req.user._id,req.params.id);
+        BaseController.logAction('DELETE_ADDRESS', address.user);
+        BaseController.sendSuccess(res,res.message)
+    })
+}
+
+module.exports = AddressController;
