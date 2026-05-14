9583f7705f7ffb840bae0241f89398cce6b9df62
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2026-05-14 18:32:17 +0530

    sidebar added to home page

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -1,35 +1,25 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
+import Sidebar from "../../components/ui/Sidebar";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const { user, logout } = useAuth();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login", { replace: true });
-  };
+  const { user } = useAuth();
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">
-        Welcome {user?.name} 
-      </h1>
-      <p className="text-gray-600 mb-6">You are on home page.</p>
-<div className="flex gap-3">
-      <button
-        onClick={handleLogout}
-        className="bg-[#F83758] text-white px-6 py-2 rounded-lg hover:opacity-90 transition"
-      >
-        Logout
-      </button>
-      <Link
-        to="/profile"
-        className="bg-[#F83758] text-white px-4 py-2 rounded-lg"
-      >
-        MyProfle
-      </Link>
+    <div className="min-h-[calc(100vh-101px)] flex items-center  bg-gray-100">
+      <Sidebar/>
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Welcome {user?.name}</h1>
+        <p className="text-gray-600 mb-6">You are on home page.</p>
+        <div className="flex gap-3">
+          <Link
+            to="/profile"
+            className="bg-[#F83758] text-white px-4 py-2 rounded-lg"
+          >
+            MyProfle
+          </Link>
+        </div>
       </div>
     </div>
   );
